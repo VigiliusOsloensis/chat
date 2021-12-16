e0eb02e596d3f39f15eb59b3c3be91829ccf460c
VigiliusOsloensis <VigiliusOsloensis@protonmail.com>
2021-12-16 21:54:08 +0100

    CORS test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ const io = require('socket.io')(server, {
   });
 const port = 8080;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   // Add this
+   if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+        res.header('Access-Control-Max-Age', 120);
+        return res.status(200).json({});
+    }
+    next();
+});
+
 io.on('connection', (socket) => {
     console.log("connected");
     socket.join(socket.id);
