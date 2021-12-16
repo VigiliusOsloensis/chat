63e171e708b06ab3e6bcc29c14d8c4e8cec335dc
VigiliusOsloensis <VigiliusOsloensis@protonmail.com>
2021-12-16 20:55:57 +0100

    stuff

--- ninehells/src/environments/environment.ts
+++ ninehells/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://infinite-falls-12171.herokuapp.com'
 };
 
 /*
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
       origin: '*',
+      methods: [GET,POST,OPTIONS,DELETE,PUT]
     }
   });
 const port = 8080;
