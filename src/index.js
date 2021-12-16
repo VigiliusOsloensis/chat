0c996be2a055659887a3f0c653d43eaa703c557d
VigiliusOsloensis <VigiliusOsloensis@protonmail.com>
2021-12-16 22:01:34 +0100

    another cors fix attempt

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   // Add this
-   if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-        res.header('Access-Control-Max-Age', 120);
-        return res.status(200).json({});
-    }
-    next();
-});
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-      origin: '*',
+      origin: ['http://localhost:4200', '*'],
       methods: ["GET", "POST"],
       credentials: true
     }
   });
 const port = 8080;
+var cors = require('cors');
+app.use(cors());
 
 
 
